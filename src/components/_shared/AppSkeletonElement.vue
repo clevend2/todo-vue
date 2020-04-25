5fcdeb872809cc169e990605e18c289002eb2be2
Daniel Clevenger <daniel.l.clevenger@gmail.com>
2020-04-25 18:02:28 +0000

    WIP on skeleton element for skeleton screen on note load

---
+++ src/components/_shared/AppSkeletonElement.vue
@@ -0,0 +1,44 @@
+<template functional>
+  <div
+    class="skeleton-container"
+    :class="{'show': contentReady}"
+  >
+    <div
+      class="skeleton-element"
+      :class="data.class"
+    />
+    <slot :class="data.class" />
+  </div>
+</template>
+
+<script>
+export default {
+  functional: true,
+  inheritAttrs: false,
+  props: {
+    contentReady: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@use '@/assets/scss/_colors' as colors;
+@use '@/assets/scss/_spacing' as spacing;
+@use '@/assets/scss/_mixins' as decor;
+
+.skeleton-container {
+  background-color: colors.$bg-secondary;
+  margin: spacing.$text;
+
+  >:not(.skeleton-element), &.show>.skeleton-element {
+    display: none;
+  }
+
+  &.show>:not(.skeleton-element) {
+    display: initial;
+  }
+}
+</style>
